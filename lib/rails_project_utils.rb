bcb974b4cfb5ebdd1a4fbbef777ca9ebdfff6275
Atilla Gündüz <gunduz.atilla@icloud.com>
2024-03-09 11:16:33 +0300

    initial commit

---
+++ lib/rails_project_utils.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rails_project_utils/version"
+
+module RailsProjectUtils
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rails_project_utils/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RailsProjectUtils
+  VERSION = "0.1.0"
+end
