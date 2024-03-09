d1b1d4bb4a0d5d334cb2cf1b227e178c88901ba4
Atilla Gündüz <gunduz.atilla@icloud.com>
2024-03-09 14:28:01 +0300

    add bin/scripts folder the generator

--- lib/generators/rails_project_utils_generator.rb
+++ lib/generators/rails_project_utils_generator.rb
@@ -1,17 +1,27 @@
 # lib/generators/rails_project_utils_generator.rb
 
 require 'rails/generators'
-require 'bundler/cli'
+require 'fileutils'
 
 module RailsProjectUtils
   module Generators
     class RailsProjectUtilsGenerator < Rails::Generators::Base
       def install_rails_project_utils
-        say 'Installing rails_project_utils gem...'
-        Bundler::CLI.start(['gem', 'install', 'rails_project_utils', '--no-document'])
+        say 'Copying run script to bin folder...'
+        source_run = File.expand_path('../templates/bin/run', __FILE__)
+        destination_run = Rails.root.join('bin', 'run')
+        FileUtils.cp(source_run, destination_run)
+        FileUtils.chmod('+x', destination_run)
 
-        say 'Running install_script.rb...'
-        system('ruby exe/install_script.rb')
+        say 'Copying scripts folder to bin/scripts...'
+        source_scripts = File.expand_path('../templates/bin/scripts', __FILE__)
+        destination_scripts = Rails.root.join('bin', 'scripts')
+        FileUtils.cp_r(source_scripts, destination_scripts)
+
+        say 'Setting executable permission for scripts...'
+        Dir.glob(destination_scripts.join('*')).each do |script|
+          FileUtils.chmod('+x', script)
+        end
       end
     end
   end
