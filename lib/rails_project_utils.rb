e38aba0f2f2ab0362521a0364e287579612d2e88
Atilla Gündüz <gunduz.atilla@icloud.com>
2024-03-09 15:27:23 +0300

    trying post install message

--- lib/rails_project_utils.rb
+++ lib/rails_project_utils.rb
@@ -4,8 +4,8 @@ require_relative "rails_project_utils/version"
 
 module RailsProjectUtils
   class Error < StandardError; end
+end
 
-  Gem.post_install do |installer|
-    puts "My post-install message for #{installer.spec.name}"
-  end
+Gem.post_install do |installer|
+  puts "My post-install message for #{installer.spec.name}"
 end
